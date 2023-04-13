a87f176ad384dd6ccae751a97d2ca2f5c32fec15
JorgeAraya99 <jorge.araya.v@ug.uchile.cl>
2023-04-12 20:33:24 -0400

    movimiento personaje

--- game/scenes/player.gd
+++ game/scenes/player.gd
@@ -1,38 +1,46 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-const GRAVITY = 500
-const ACCELERATION = 4000
+#const SPEED = 300.0
+#const JUMP_VELOCITY = -400.0
+#const GRAVITY = 500
+#const ACCELERATION = 4000
+#
+## Get the gravity from the project settings to be synced with RigidBody nodes.
+#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+#
+#
+#func _ready():
+#	pass
+#
+#func _process(delta):
+#	pass
+#
+#func _physics_process(delta):
+#	# Add the gravity.
+#	if not is_on_floor():
+#		velocity.y += GRAVITY * delta
+#
+#	# Handle Jump.
+#	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+#		velocity.y = JUMP_VELOCITY
+#
+#	# Get the input direction and handle the movement/deceleration.
+#	# As good practice, you should replace UI actions with custom gameplay actions.
+#	var direction = Input.get_axis("ui_left", "ui_right")
+#	if direction:
+#		velocity.x = direction * SPEED
+#
+#	var move_input = Input.get_axis("move_left", "move_right")
+#
+#	velocity.x = move_toward(velocity.x, move_input*SPEED,ACCELERATION*delta)
+#
+#	move_and_slide()
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var SPEED : float = 100
 
+func _physics_process(_delta):
+	var input_direction = Vector2(Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),Input.get_action_strength("move_down") - Input.get_action_strength("move_up"))
 
-func _ready():
-	pass
-
-func _process(delta):
-	pass
-
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += GRAVITY * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	
-	var move_input = Input.get_axis("move_left", "move_right")
-	
-	velocity.x = move_toward(velocity.x, move_input*SPEED,ACCELERATION*delta)
-
+	velocity = input_direction * SPEED
 	move_and_slide()
