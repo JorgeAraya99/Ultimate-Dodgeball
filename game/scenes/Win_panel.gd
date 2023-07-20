199b871623e04908f00e5415101eb5231fc4ffdc
JorgeAraya99 <jorge.araya.v@ug.uchile.cl>
2023-07-20 18:11:33 -0400

    cambios de menu de victoria y creditos

--- game/scenes/Win_panel.gd
+++ game/scenes/Win_panel.gd
@@ -10,10 +10,5 @@ func _ready():
 func _process(delta):
 	pass
 
-
-func _on_restart_pressed():
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
-
-
 func _on_back_to_menu_pressed():
 	get_tree().change_scene_to_file("res://scenes/MainMenu.tscn")
